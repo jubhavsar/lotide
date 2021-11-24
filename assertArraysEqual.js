1cb28db54a3e0998e55a450a46bd0e0d95e85f42
jubhavsar <juhi_bhavsar@yahoo.com>
2021-11-23 20:31:28 -0500

    Check identical array

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,9 +18,6 @@ const eqArrays = (array1, array2) => {
 
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 1, 1]), false); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([], []), true); // => true
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false); // => true
