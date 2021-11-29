f6f55181b8f50ba5f3bd6bf146b0467a5ed8aad3
jubhavsar <juhi_bhavsar@yahoo.com>
2021-11-29 15:16:24 -0500

    Using import/export

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) =>{
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
@@ -18,9 +12,4 @@ const eqArrays = (array1, array2) => {
 
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 1, 1]), false); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
