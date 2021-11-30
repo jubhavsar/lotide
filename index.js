c3292d6971c40663be3b4b9c32397d10b77cca9f
jubhavsar <juhi_bhavsar@yahoo.com>
2021-11-29 21:30:57 -0500

    List of all functions

--- index.js
+++ index.js
@@ -2,11 +2,40 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const asserEqual = require('./assertEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly')
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const map = require('./map');
+
+
 
 module.exports = {
     head,
     tail,
    middle,
-   asserEqual
+   assertEqual,
+   assertArraysEqual,
+   eqArrays,
+   eqObjects,
+   findKey,
+   assertObjectsEqual,
+   findKeyByValue,
+   flatten,
+   letterPositions,
+   countLetters,
+   countOnly,
+   takeUntil,
+   without,
+   map
+
 };
