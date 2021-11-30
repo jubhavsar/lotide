413bd5ca75c025d113985525bfa366e524362330
jubhavsar <juhi_bhavsar@yahoo.com>
2021-11-29 20:00:03 -0500

    Add assertEqual method

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const asserEqual = require('./assertEqual');
 
 module.exports = {
     head,
     tail,
-   middle
+   middle,
+   asserEqual
 };
